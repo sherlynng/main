e2f10c688020c08a9bf8b3d27cd7ae32a2011e87
Aussiroth <aussiroth@gmail.com>
2018-03-01 22:57:44 +0800

    Add in JUnit test for using alias of add command.

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -49,6 +49,13 @@ public class AddressBookParserTest {
     }
 
     @Test
+    public void parseCommand_addAliased() throws Exception {
+        Person person = new PersonBuilder().build();
+        AddCommand command = (AddCommand) parser.parseCommand(PersonUtil.getAddCommandAliased(person));
+        assertEquals(new AddCommand(person), command);
+    }
+
+    @Test
     public void parseCommand_clear() throws Exception {
         assertTrue(parser.parseCommand(ClearCommand.COMMAND_WORD) instanceof ClearCommand);
         assertTrue(parser.parseCommand(ClearCommand.COMMAND_WORD + " 3") instanceof ClearCommand);
