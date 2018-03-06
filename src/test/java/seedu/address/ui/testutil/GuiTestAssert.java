1ead03da511e02d92417aa19da0dfed8d601b97b
Danny Ng <danny.ng94@gmail.com>
2018-03-06 22:37:38 +0800

    Fixed typo

--- src/test/java/seedu/address/ui/testutil/GuiTestAssert.java
+++ src/test/java/seedu/address/ui/testutil/GuiTestAssert.java
@@ -54,28 +54,28 @@ public class GuiTestAssert {
      */
     private static String getTagColorStyleFor(String tagName) {
         switch (tagName) {
-        case "classmates":
-        case "owesMoney":
-            return "teal";
+            case "classmates":
+            case "owesMoney":
+                return "teal";
 
-        case "colleagues":
-        case "neighbours":
-            return "yellow";
+            case "colleagues":
+            case "neighbours":
+                return "yellow";
 
-        case "family":
-        case "friend":
-            return "orange";
+            case "family":
+            case "friend":
+                return "orange";
 
-        case "friends":
-            return "brown";
+            case "friends":
+                return "brown";
+
+            case "husband":
+                return "grey";
+
+            default:
+                fail(tagName + " does not have a color assigned.");
+                return "";
         }
-
-        case "husband":
-            return "grey";
-
-        default:
-            fail(tagName + " does not have a color assigned.");
-            return "";
     }
 
     /**
