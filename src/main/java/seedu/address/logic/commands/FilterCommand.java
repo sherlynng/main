db7386cfe9f1da091d7252cc97b02195dec27ddf
Danny Ng <danny.ng94@gmail.com>
2018-03-20 15:36:22 +0800

    Add filter command

---
+++ src/main/java/seedu/address/logic/commands/FilterCommand.java
@@ -0,0 +1,38 @@
+package seedu.address.logic.commands;
+
+import java.util.function.Predicate;
+
+import seedu.address.model.person.Person;
+
+/**
+ * Finds and lists all persons in address book whose name contains any of the argument keywords.
+ * Keyword matching is case sensitive.
+ */
+public class FilterCommand extends Command {
+
+    public static final String COMMAND_WORD = "filter";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Filter all persons whose tag contain any of "
+            + "the specified predicate and displays them as a list with index numbers.\n"
+            + "Parameters: TAG\n"
+            + "Example: " + COMMAND_WORD + " math";
+
+    private final Predicate<Person> predicate;
+
+    public FilterCommand(Predicate<Person> predicate) {
+        this.predicate = predicate;
+    }
+
+    @Override
+    public CommandResult execute() {
+        model.updateFilteredPersonList(predicate);
+        return new CommandResult(getMessageForPersonListShownSummary(model.getFilteredPersonList().size()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof FilterCommand // instanceof handles nulls
+                && this.predicate.equals(((FilterCommand) other).predicate)); // state check
+    }
+}
