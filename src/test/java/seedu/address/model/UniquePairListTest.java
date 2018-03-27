8356823a71f5a4a17785a01b2febf0fdccfc019a
alexawangzi <alexawangzi@gmail.com>
2018-03-27 21:51:50 +0800

    updated UniquePairListTest

--- src/test/java/seedu/address/model/UniquePairListTest.java
+++ src/test/java/seedu/address/model/UniquePairListTest.java
@@ -1,12 +1,18 @@
 package seedu.address.model;
 
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static seedu.address.testutil.TypicalPairs.ALICE_AND_BENSON;
+import static seedu.address.testutil.TypicalPairs.CARL_AND_DANIEL;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+
 import seedu.address.model.pair.UniquePairList;
+import seedu.address.model.pair.exceptions.DuplicatePairException;
 
 public class UniquePairListTest {
-    
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
@@ -16,4 +22,20 @@ public class UniquePairListTest {
         thrown.expect(UnsupportedOperationException.class);
         uniquePairList.asObservableList().remove(0);
     }
+
+    @Test
+    public void checkContainPair() throws DuplicatePairException {
+        UniquePairList uniquePairList = new UniquePairList();
+        uniquePairList.add(ALICE_AND_BENSON);
+        assertTrue(uniquePairList.contains(ALICE_AND_BENSON));
+        assertFalse(uniquePairList.contains(CARL_AND_DANIEL));
+    }
+
+    @Test
+    public void addPair() throws DuplicatePairException {
+        UniquePairList uniquePairList = new UniquePairList();
+        uniquePairList.add(ALICE_AND_BENSON);
+        assertTrue(uniquePairList.contains(ALICE_AND_BENSON));
+    }
+
 }
