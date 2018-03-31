3e9281da18a345627acf7de47ca50dfc69779d47
Aussiroth <aussiroth@gmail.com>
2018-03-31 23:00:11 +0800

    Fix checkstyle issues.

--- src/main/java/seedu/address/logic/predicates/FindMissingPredicate.java
+++ src/main/java/seedu/address/logic/predicates/FindMissingPredicate.java
@@ -1,8 +1,12 @@
 package seedu.address.logic.predicates;
 
 import java.util.function.Predicate;
+
 import seedu.address.model.person.Person;
 
+/**
+ * Tests that a {@code Person}'s specified {@code Attribute} as in given value attribute is an empty string.
+ */
 public class FindMissingPredicate implements Predicate<Person> {
     private final String attribute;
 
@@ -13,24 +17,25 @@ public class FindMissingPredicate implements Predicate<Person> {
     @Override
     public boolean test(Person person) {
         switch (attribute) {
-            case "email":
-                return person.getEmail().equals("");
-            case "phone":
-                return person.getPhone().equals("");
-            case "address":
-                return person.getAddress().equals("");
-            case "price":
-                return person.getPrice().equals("");
-            case "level":
-                return person.getLevel().equals("");
-            case "subject":
-                return person.getSubject().equals("");
-            case "role":
-                return person.getRole().equals("");
-            case "status":
-                return person.getStatus().equals("");
+        case "email":
+            return person.getEmail().equals("");
+        case "phone":
+            return person.getPhone().equals("");
+        case "address":
+            return person.getAddress().equals("");
+        case "price":
+            return person.getPrice().equals("");
+        case "level":
+            return person.getLevel().equals("");
+        case "subject":
+            return person.getSubject().equals("");
+        case "role":
+            return person.getRole().equals("");
+        case "status":
+            return person.getStatus().equals("");
+        default: //fallthrough
+            return false;
         }
-        return false; //fallthrough
     }
 
     @Override
