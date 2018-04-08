d27e5bb527e2b2df659e4d215e210194dbd66ab5
Aussiroth <aussiroth@gmail.com>
2018-04-08 17:46:20 +0800

    Cleanup unused code

--- src/main/java/seedu/address/logic/parser/FilterCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FilterCommandParser.java
@@ -41,13 +41,4 @@ public class FilterCommandParser implements Parser<FilterCommand> {
         }
         return allPredicates;
     }
-
-    /**
-     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
-     * {@code ArgumentMultimap}.
-     */
-    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
-        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
-    }
-
 }
