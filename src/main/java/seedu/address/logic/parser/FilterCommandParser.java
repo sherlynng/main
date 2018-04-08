59005bf811abed21ec9f564f7b1fd9443e6c3bf8
Aussiroth <aussiroth@gmail.com>
2018-04-08 17:53:52 +0800

    Fix unused import

--- src/main/java/seedu/address/logic/parser/FilterCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FilterCommandParser.java
@@ -3,7 +3,6 @@ package seedu.address.logic.parser;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
-import java.util.stream.Stream;
 
 import seedu.address.logic.commands.FilterCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
