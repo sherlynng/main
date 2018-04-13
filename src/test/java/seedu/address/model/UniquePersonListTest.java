69d6a7f569ca3daf319d8e58511ddd9092e9b62e
Aussiroth <aussiroth@gmail.com>
2018-04-13 19:27:27 +0800

    Add to UniquePersonList tests to test person not found exception.

--- src/test/java/seedu/address/model/UniquePersonListTest.java
+++ src/test/java/seedu/address/model/UniquePersonListTest.java
@@ -1,10 +1,15 @@
 package seedu.address.model;
 
+import static seedu.address.testutil.Assert.assertThrows;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import seedu.address.model.person.Person;
 import seedu.address.model.person.UniquePersonList;
+import seedu.address.model.person.exceptions.PersonNotFoundException;
+import seedu.address.testutil.TypicalPersons;
 
 public class UniquePersonListTest {
     @Rule
@@ -15,5 +20,13 @@ public class UniquePersonListTest {
         UniquePersonList uniquePersonList = new UniquePersonList();
         thrown.expect(UnsupportedOperationException.class);
         uniquePersonList.asObservableList().remove(0);
+    }
+
+    @Test
+    public void editAndDelete_noMatchingPerson_throwsPersonNotFoundException() {
+        UniquePersonList uniquePersonList = new UniquePersonList();
+        Person person = TypicalPersons.ALICE;
+        assertThrows(PersonNotFoundException.class, () -> uniquePersonList.remove(person));
+        assertThrows(PersonNotFoundException.class, () -> uniquePersonList.setPerson(person, person));
     }
 }
